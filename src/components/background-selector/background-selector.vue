ccfcfcd65a836ab97ba3f3f5af0e24455d85cbd6
ysaacnoa <ysaacnoe.correa@unmsm.edu.pe>
2025-05-20 23:21:09 -0500

    ♻️ (home): background selector

--- src/components/background-selector/background-selector.vue
+++ src/components/background-selector/background-selector.vue
@@ -10,21 +10,54 @@
         v-for="(option, index) in options"
         :key="index"
         @click="$emit('update:modelValue', option)"
+        class="menu-item"
       >
-        {{ option }}
+        <div class="flex items-center gap-3">
+          <component
+            :is="getIcon(option)"
+            class="w-4 h-4 "
+          />
+          <span class="">{{ formatOptionName(option) }}</span>
+        </div>
       </DropdownMenuItem>
     </DropdownMenuContent>
   </DropdownMenu>
 </template>
 
 <script setup lang="ts">
+import type { Component } from 'vue'
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
-import { Palette } from 'lucide-vue-next'
+import {
+  Palette,
+  Sparkles,
+  Droplets,
+  CircleDot,
+  Cpu,
+  MousePointerClick
+} from 'lucide-vue-next'
+
+const iconMap: Record<BackgroundType, Component> = {
+  [BackgroundType.BLURRED]: Sparkles,
+  [BackgroundType.BUBBLES]: Droplets,
+  [BackgroundType.CURSOR]: MousePointerClick,
+  [BackgroundType.GRADIENT]: CircleDot,
+  [BackgroundType.MESH]: Cpu,
+  [BackgroundType.MOTES]: Sparkles,
+  [BackgroundType.MOUSE]: MousePointerClick
+}
+
+function getIcon(option: BackgroundType) {
+  return iconMap[option] || Sparkles
+}
+
+function formatOptionName(option: BackgroundType) {
+  return option
+}
 import { BackgroundType } from '@/modules/background/constants'
 
 defineProps<{
