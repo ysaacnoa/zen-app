886d95069e2883a4c33cfb52fb717e6a811e20d8
ysaacnoa <ysaacnoe.correa@unmsm.edu.pe>
2025-05-20 23:06:04 -0500

    🔧 (home): landing config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}"
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,13 @@
-import { fileURLToPath, URL } from 'node:url'
 import path from 'node:path'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss()
-  ],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '~': path.resolve(__dirname, './')
+      '@': path.resolve(__dirname, './src'),
     },
-  },
+  }
 })
