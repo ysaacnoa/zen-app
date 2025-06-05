3a92ea90558078ac16fb9c0bc0910ad2fa8435a6
ysaacnoa <ysaacnoe.correa@unmsm.edu.pe>
2025-06-05 00:15:36 -0500

    ✨ (home): create base home

---
+++ src/views/home/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home-view" data-test="home-view">
+    <h1>Bienvenido a Zen App</h1>
+    <p>Tu aplicación para el bienestar mental</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: 'HomeView'
+})
+</script>
+
+<style scoped>
+.home-view {
+  text-align: center;
+  padding: 2rem;
+}
+</style>
