fe0a20486bd5d78217838a642cce7d029ae7bf25
ysaacnoa <ysaacnoe.correa@unmsm.edu.pe>
2025-06-04 20:47:13 -0500

    ♻️ (forms): configure forms view

---
+++ src/views/forms/forms.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="forms-container">
+    <GAD7Form />
+  </div>
+</template>
+
+<script setup lang="ts">
+import GAD7Form from '@/views/landing/GAD7Form.vue'
+defineOptions({
+  name: 'FormsView'
+})
+</script>
+
+<style scoped>
+.forms-container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 2rem;
+}
+</style>
