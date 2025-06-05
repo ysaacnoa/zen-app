644b6dcfa535178a2db29e51c4d1bb38f928854a
ysaacnoa <ysaacnoe.correa@unmsm.edu.pe>
2025-06-04 23:18:43 -0500

    ♻️ (forms): refactor style forms

--- src/components/ui/card/Card.vue
+++ src/components/ui/card/Card.vue
@@ -12,7 +12,7 @@ const props = defineProps<{
     data-slot="card"
     :class="
       cn(
-        'bg-card text-card-foreground flex flex-col gap-2 rounded-xl border py-6 shadow-sm',
+        'text-card-foreground flex flex-col gap-2 rounded-xl py-6',
         props.class,
       )
     "
