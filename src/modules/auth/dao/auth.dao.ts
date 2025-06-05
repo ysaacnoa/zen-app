36cffdaade62d7657d57e4a8c61b01bbc28cf23e
ysaacnoa <ysaacnoe.correa@unmsm.edu.pe>
2025-06-04 23:54:21 -0500

    ♻️ (auth): simplify auth module

--- src/modules/auth/dao/auth.dao.ts
+++ src/modules/auth/dao/auth.dao.ts
@@ -1,43 +1,47 @@
-import { AuthResponseSchema, type AuthResponse } from '../models/auth-response.schema'
-import { ApiService } from '@/lib/api.service'
-import AuthTokenService from '@/lib/auth-token.service'
-import { userDAO } from '@/modules/user/dao/user.dao'
-import type { UserProfile } from '@/modules/user/models/user-profile.schema'
+import { AuthResponseSchema, type AuthResponse } from '../models/auth-response.schema';
+import { ApiService } from '@/lib/api.service';
+import AuthTokenService from '@/lib/auth-token.service';
 
-const API_BASE_URL = 'http://localhost:3000/auth'
+const API_BASE_URL = 'http://localhost:3000/auth';
 
 export class AuthDAO {
   isAuthenticated(): boolean {
-    return !!AuthTokenService.getToken()
+    return !!AuthTokenService.getToken();
   }
-  private apiService: ApiService
+  private apiService: ApiService;
 
   constructor() {
-    this.apiService = new ApiService(API_BASE_URL)
+    this.apiService = new ApiService(API_BASE_URL);
   }
 
-  async login(email: string, password: string): Promise<{auth: AuthResponse, profile: UserProfile}> {
-    const auth = await this.apiService.post('/login', { email, password }, AuthResponseSchema)
-    AuthTokenService.setToken(auth.access_token)
-    const profile = await userDAO.getProfile()
-    return { auth, profile }
+  async login(
+    email: string,
+    password: string,
+  ): Promise<AuthResponse> {
+    const auth = await this.apiService.post('/login', { email, password }, AuthResponseSchema);
+    AuthTokenService.setToken(auth.access_token);
+    return auth;
   }
 
   async register(
     firstName: string,
     lastName: string,
     email: string,
-    password: string
+    password: string,
   ): Promise<AuthResponse> {
-    const response = await this.apiService.post('/register', {
-      firstName,
-      lastName,
-      email,
-      password
-    }, AuthResponseSchema)
-    AuthTokenService.setToken(response.access_token)
-    return response
+    const response = await this.apiService.post(
+      '/register',
+      {
+        firstName,
+        lastName,
+        email,
+        password,
+      },
+      AuthResponseSchema,
+    );
+    AuthTokenService.setToken(response.access_token);
+    return response;
   }
 }
 
-export const authDAO = new AuthDAO()
+export const authDAO = new AuthDAO();
