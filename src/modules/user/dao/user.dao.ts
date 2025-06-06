ab9f94d448238a1736642488b6bf1e55abf57684
ysaacnoa <ysaacnoe.correa@unmsm.edu.pe>
2025-06-06 00:09:45 -0500

    ♻️ (user): mmodify dao and store

--- src/modules/user/dao/user.dao.ts
+++ src/modules/user/dao/user.dao.ts
@@ -1,8 +1,7 @@
 import { UserProfileSchema, type UserProfile } from '../models/user-profile.schema'
 import { ApiService } from '@/lib/api.service'
-import AuthTokenService from '@/lib/auth-token.service'
 
-const API_BASE_URL = 'http://localhost:3000/user'
+const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/user`
 
 export class UserDAO {
   private apiService: ApiService
@@ -12,15 +11,7 @@ export class UserDAO {
   }
 
   async getProfile(): Promise<UserProfile> {
-    try {
-      return await this.apiService.get('/me', UserProfileSchema)
-    } catch (error) {
-      if (error instanceof Error && error.message.includes('401')) {
-        AuthTokenService.clearToken()
-        throw new Error('Session expired. Please login again.')
-      }
-      throw error
-    }
+    return this.apiService.get('/me', UserProfileSchema)
   }
 }
 
