564c04cfd47663b9bdbf0edf247a54dcaa9096c9
ysaacnoa <ysaacnoe.correa@unmsm.edu.pe>
2025-06-06 20:51:44 -0500

    💄 (challenges): update styles

--- src/views/home/challenges/challenges.vue
+++ src/views/home/challenges/challenges.vue
@@ -1,45 +1,43 @@
 <template>
   <div class="challenges-view">
-    <div v-if="challengeStore.loading">Loading challenges...</div>
-    <div v-else-if="challengeStore.error" class="error">
-      {{ challengeStore.error }}
-    </div>
-    <div v-else class="challenges-grid">
-      <div
-        v-for="challenge in challengeStore.pendingChallenges"
-        :key="challenge.id"
-        class="challenge-card"
-        :class="`type-${challenge.type.toLowerCase()}`"
-      >
-        <div class="card-header">
-          <h3>{{ challenge.title }}</h3>
-          <div class="challenge-type">{{ challenge.type }}</div>
-        </div>
-
-        <div class="card-content">
-          <p>{{ challenge.instructions }}</p>
-          <div class="stats">
-            <div>Completions: {{ challenge.completionCount }}/{{ challenge.requiredCompletions }}</div>
-            <div>XP Reward: {{ challenge.rewardXp }}</div>
+    <router-view v-if="$route.name === 'challenge-detail'"/>
+    <template v-else>
+      <div v-if="challengeStore.loading">Loading challenges...</div>
+      <div v-else-if="challengeStore.error" class="error">
+        {{ challengeStore.error }}
+      </div>
+      <div v-else class="challenges-grid">
+        <div
+          v-for="challenge in challengeStore.pendingChallenges"
+          :key="challenge.id"
+          class="challenge-card"
+          :class="`type-${challenge.type.toLowerCase()}`"
+          @click="viewChallenge(challenge.id)"
+        >
+          <div class="card-header">
+            <h3>{{ challenge.title }}</h3>
+            <div class="challenge-type">{{ challenge.type }}</div>
           </div>
-        </div>
 
-        <div class="card-footer">
-          <div class="progress-bar">
-            <div
-              class="progress-fill"
-              :style="{ width: `${(challenge.completionCount / challenge.requiredCompletions) * 100}%` }"
-            ></div>
+          <div class="card-content">
+            <p>{{ challenge.instructions }}</p>
+            <div class="stats">
+              <div>Completions: {{ challenge.completionCount }}/{{ challenge.requiredCompletions }}</div>
+              <div>XP Reward: {{ challenge.rewardXp }}</div>
+            </div>
           </div>
-          <button
-            class="complete-btn"
-            @click="completeChallenge(challenge.id)"
-          >
-            Complete
-          </button>
+
+          <div class="card-footer">
+            <div class="progress-bar">
+              <div
+                class="progress-fill"
+                :style="{ width: `${(challenge.completionCount / challenge.requiredCompletions) * 100}%` }"
+              ></div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </template>
   </div>
 </template>
 
@@ -47,6 +45,7 @@
 import { useChallengeStore } from '@/modules/gamification/challenges/stores/challenge.store'
 import { onMounted } from 'vue'
 import { useUserStore } from '@/modules/user/stores/user.store'
+import { router } from '@/modules/routes'
 
 const userStore = useUserStore()
 const challengeStore = useChallengeStore()
@@ -62,8 +61,8 @@ onMounted(async () => {
   }
 })
 
-async function completeChallenge(challengeId: string) {
-  await challengeStore.completeChallenge(challengeId)
+async function viewChallenge(challengeId: string) {
+  await router.push({name: 'challenge-detail', params:{id: challengeId}})
 }
 </script>
 
