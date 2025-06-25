b3273a193776feeb2918e2b032c490250e43bcd7
ysaacnoa <ysaacnoe.correa@unmsm.edu.pe>
2025-06-25 11:51:02 -0500

    🚑 (charts): fix imports

--- src/views/home/analytics/index.ts
+++ src/views/home/analytics/index.ts
@@ -1,4 +1,1 @@
 export { default as AnalyticsDashboard } from './analytics.vue'
-export { default as StatsChart } from './components/StatsChart.vue'
-export { default as TypesChart } from './components/TypesChart.vue'
-export { default as TrendsChart } from './components/TrendsChart.vue'
